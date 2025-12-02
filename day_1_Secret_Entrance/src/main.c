d91db05bed57fd9bc9148b7756c4c1a49eaf7faf
Laurens Moonens <laurens32.moonens@telenet.be>
2025-12-02 15:51:14 +0100

    Day 1 part 2: Updated to also count the amount of times we passed by zero without ending there.

--- day_1_Secret_Entrance/src/main.c
+++ day_1_Secret_Entrance/src/main.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,7 +10,8 @@ const int MAX_DIAL_NUMBER = 99;
 int main()
 {
     int currentDialNumber = 50;
-    int numberOfZeros = 0;
+    int numberOfZerosEnded = 0;
+    int numberOfZerosPassed = 0;
 
     FILE* file;
     char* filePath = "../resources/input.txt";
@@ -18,6 +20,7 @@ int main()
     if (file == NULL)
     {
         printf("Unable to open file at location '%s'\n", filePath);
+        exit(1);
     }
 
     char line[6] = {0};
@@ -30,22 +33,41 @@ int main()
         int dialChange = atoi(&line[1]);
         dialChange *= dialChangeSign;
 
+        if (dialChange == 0)
+        {
+            continue;
+        }
+
+        int prevNumberOfZerosEnded = numberOfZerosEnded;
+        int prevNumberOfZerosPassed = numberOfZerosPassed;
+        bool startedAtZero = currentDialNumber == 0;
+
         currentDialNumber += dialChange;
 
         while (currentDialNumber > MAX_DIAL_NUMBER || currentDialNumber < MIN_DIAL_NUMBER)
         {
             currentDialNumber -= ((MAX_DIAL_NUMBER - MIN_DIAL_NUMBER) + 1) * dialChangeSign;
+            numberOfZerosPassed++;
+        }
+
+        if ((startedAtZero && dialChangeSign == -1) || (currentDialNumber == 0 && dialChangeSign == 1))
+        {
+            numberOfZerosPassed--;
         }
 
         if (currentDialNumber == 0)
         {
-            numberOfZeros++;
+            numberOfZerosEnded++;
         }
+
+        assert(prevNumberOfZerosEnded <= numberOfZerosEnded);
+        assert(prevNumberOfZerosPassed <= numberOfZerosPassed);
     }
 
     fclose(file);
 
-    printf("%d\n", numberOfZeros);
+    printf("Number of times ended at zero = %d\n", numberOfZerosEnded);
+    printf("Number of times passed by zero AND ended at zero = %d\n", numberOfZerosPassed + numberOfZerosEnded);
 
     return 0;
 }
